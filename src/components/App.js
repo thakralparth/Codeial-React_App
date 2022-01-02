7949d4c20a42a349443c6ea1e7d193572b9ab281
Parth Thakral <parth.thakral72@gmail.com>
2022-01-02 21:14:25 +0530

    displaying posts on home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Loader } from '.';
 import { getPosts } from '../api';
 import { Home } from '../pages'
 
 
 function App() {
+  const [posts, setPosts] =  useState([]);
+  const [loading, setLoading]=useState(true);
+
+
   useEffect(()=>{
     const fetchPosts = async ()=>{
       const response = await getPosts();
-      console.log('response',response);
+      // console.log('response',response);
+      if(response.success){
+        setPosts(response.data.posts);
+      }
+
+      setLoading(false);
     }
 
     fetchPosts();
 
   },[]);
+
+  if (loading){
+    return <Loader />
+  }
   return (
     <div className="App">
       {/* <h1>Hello World</h1> */}
-    <Home />
+    <Home posts={posts}/>
     </div>
   );
 }
