d1f71aea1d8c5ff1aaaa0c3f26024e4ed6a690bf
Parth Thakral <parth.thakral72@gmail.com>
2022-01-02 16:53:47 +0530

    Set up directories, API and custom fetch function

---
+++ src/api/index.js
@@ -0,0 +1,53 @@
+import { API_URLS,LOCALSTORAGE_TOKEN_KEY } from "../utils";
+
+const customFetch= async (url,{ body , ...customConfig })=>{    //body -> userid/password
+    const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);   //get token from API after logging in for storing in local storgae
+
+    const headers={
+        'content-type': 'application/json',
+        Accept: 'application/json'
+    }
+
+    if(token){
+        headers.Authorization = `Bearer ${token}`;
+    }
+
+    const config={
+        ...customConfig,
+        headers:{
+            ...headers,
+            ...customConfig.headers,
+        }
+    }
+    
+    if(body){
+        config.body = JSON.stringify(body);
+    }
+    
+
+
+    try{
+        const response = await fetch(url,config);
+        const data= await response.json();
+
+        if(data.success){
+            return {
+                data:data.data,
+                success: true,
+            }
+        }
+        throw new Error(data.message);
+    }catch(error){
+        console.log(error);
+        return {
+            message:error.message,
+            success: false,
+        }
+    }
+};
+
+export const getPosts=(page=1,limit=5)=>{
+    return customFetch(API_URLS.posts(page, limit),{
+        method: 'GET',
+    });
+};
